dcb56d8faef6788f726437a4bb1a9b05f9d74b08
subhamukherjee321 <subhamukherjee321@gmail.com>
2024-06-21 16:57:21 +0530

    Last section is done for mobile and desktop screen

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,4 +1,6 @@
 import HeaderSection from "@/components/HeaderSection";
+import GridSection from "@/components/LastSection/GridSection";
+import SwiperSection from "@/components/LastSection/SwiperSection";
 import MiddleSection from "@/components/MiddleSection/MiddleSection";
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
       <header>
         <HeaderSection />
         <MiddleSection />
+        <SwiperSection />
+        <GridSection />
       </header>
     </main>
   );
